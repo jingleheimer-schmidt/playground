428043074fd4079a597fbac8453c6f895a5d6bf1
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2022-03-22 00:02:28 -0700

    batch processing for active/sleeping biters

--- asher_sky_testing_2/control.lua
+++ asher_sky_testing_2/control.lua
@@ -3,6 +3,8 @@
 Spider Trails control script © 2022 by asher_sky is licensed under Attribution-NonCommercial-ShareAlike 4.0 International. See LICENSE.txt for additional information
 --]]
 
+local table = require("__flib__.table")
+
 local speeds = {
   veryslow = 0.010,
   slow = 0.025,
@@ -26,14 +28,15 @@ local pi_4 = 4 * math.pi / 3
 
 function make_rainbow(event_tick, unit_number, frequency, palette_choice)
   -- local frequency = speeds[settings["biter-trails-speed"]]
-  local modifier = unit_number + event_tick
+  -- local modifier = unit_number + event_tick
+  local freq_mod = frequency * (unit_number + event_tick)
   -- local palette_choice = palette[settings["biter-trails-palette"]]
   local amplitude = palette_choice.amplitude
   local center = palette_choice.center
   return {
-    r = sin(frequency*(modifier)+pi_0)*amplitude+center,
-    g = sin(frequency*(modifier)+pi_2)*amplitude+center,
-    b = sin(frequency*(modifier)+pi_4)*amplitude+center,
+    r = sin(freq_mod+pi_0)*amplitude+center,
+    g = sin(freq_mod+pi_2)*amplitude+center,
+    b = sin(freq_mod+pi_4)*amplitude+center,
     a = 255,
   }
 end
@@ -58,6 +61,9 @@ local function get_all_biters()
   if not global.biters then
     global.biters = {}
   end
+  if not global.sleeping_biters then
+    global.sleeping_biters = {}
+  end
   for each, surface in pairs(game.surfaces) do
     local biters = surface.find_entities_filtered{type={"unit"}, force={"enemy"}}
     for every, biter in pairs(biters) do
@@ -80,10 +86,10 @@ local function add_biter(event)
   -- game.print("robot added: "..robot.unit_number)
 end
 
-script.on_event(defines.events.on_built_entity, function(event)
-  add_biter(event)
-end,
-{{filter = "type", type = "unit"}})
+-- script.on_event(defines.events.on_built_entity, function(event)
+--   add_biter(event)
+-- end,
+-- {{filter = "type", type = "unit"}})
 
 -- script.on_event(defines.events.on_built_entity, function(event)
 --   add_biter(event)
@@ -100,10 +106,10 @@ end,
 -- end,
 -- {{filter = "type", type = {"construction-robot", "logistic-robot"}}})
 
-script.on_event(defines.events.script_raised_built, function(event)
-  add_biter(event)
-end,
-{{filter = "type", type = "unit"}})
+-- script.on_event(defines.events.script_raised_built, function(event)
+--   add_biter(event)
+-- end,
+-- {{filter = "type", type = "unit"}})
 
 -- script.on_event(defines.events.script_raised_built, function(event)
 --   add_biter(event)
@@ -145,83 +151,111 @@ local function make_trails(settings, event)
     local frequency = speeds[settings["biter-trails-speed"]]
     local palette_choice = palette[settings["biter-trails-palette"]]
     -- local tiptoe_mode = settings["biter-trails-tiptoe-mode"]
+    if global.sleeping_biters then
+      local sleeping_biters = global.sleeping_biters
+      global.from_key = table.for_n_of(sleeping_biters, global.from_key, 234, function(data, key)
+        if data.biter and data.biter.valid then
+          local last_position = data.position
+          local current_position = data.biter.position
+          local same_position = last_position and (last_position.x == current_position.x) and (last_position.y == current_position.y)
+          if not same_position then
+            global.biters[data.biter.unit_number] = {
+              biter = data.biter,
+              position = current_position,
+              counter = 1
+            }
+            global.sleeping_biters[data.biter.unit_number] = nil
+          end
+        end
+      end)
+    end
     local biters = global.biters
     if biters then
-      -- game.print("robots exist")
+      local new_biter_data = {}
       for unit_number, data in pairs(biters) do
-        -- game.print(game.tick..": robot "..unit_number.." exists")
         local biter = data.biter
         if not biter.valid then
-          -- game.print("robot invalid: "..unit_number)
           global.biters[unit_number] = nil
         else
-          if biter.active then
-            local last_position = data.position
-            local current_position = biter.position
-            local same_position = last_position and (last_position.x == current_position.x) and (last_position.y == current_position.y)
-            if not same_position then
-            -- if biter.moving then
-              -- game.print(game.tick.."robot: "..unit_number.." checked")
-              -- game.print(game.tick.."robot: "..unit_number.." position: "..serpent.block(robot.position))
-              local event_tick = event.tick
-              -- local color = {}
-              -- if color_mode == "biter" then
-              --   color = biter.color
-              --   color.a = 1
-              -- else
-              --   color = make_rainbow(event_tick, unit_number, settings, frequency, palette_choice)
-              -- end
-              local color = make_rainbow(event_tick, unit_number, frequency, palette_choice)
-              -- local color = {.5,.5,.5}
-              -- local last_position = data.position
-              -- local counter = data.counter
-              -- local current_position = biter.position
-              local surface = biter.surface
-              -- local same_position = last_position and (last_position.x == current_position.x) and (last_position.y == current_position.y)
-              -- local counter_is_less_than_goal = counter and (counter < 15)
-              -- game.print(game.tick.."robot: "..unit_number.." checked again 1")
-              -- if ((not same_position) or (same_position and counter_is_less_than_goal)) then
-                -- game.print(game.tick.."robot: "..unit_number.." checked again 2")
-              -- if biter.effective_speed > 0 then
-              -- if not same_position then
-              if sprite then
-                -- sprite = rendering.draw_sprite{
-                rendering.draw_sprite{
-                  sprite = "biter-trail",
-                  target = current_position,
-                  surface = surface,
-                  x_scale = scale,
-                  y_scale = scale,
-                  render_layer = "radius-visualization",
-                  time_to_live = length,
-                  tint = color,
-                }
-              end
-              if light then
-                -- light = rendering.draw_light{
-                rendering.draw_light{
-                  sprite = "biter-trail",
-                  target = current_position,
-                  surface = surface,
-                  intensity = .175,
-                  scale = scale * 1.75,
-                  render_layer = "light-effect",
-                  time_to_live = length,
-                  color = color,
-                }
-              end
-            end
-            global.biters[unit_number].position = current_position
-            -- if counter and counter < 15 then
-            --   global.biters[unit_number].counter = counter + 1
+          local last_position = data.position
+          local current_position = biter.position
+          local same_position = last_position and (last_position.x == current_position.x) and (last_position.y == current_position.y)
+          if not same_position then
+            local event_tick = event.tick
+            -- local color = {}
+            -- if color_mode == "biter" then
+            --   color = biter.color
+            --   color.a = 1
             -- else
-            --   global.biters[unit_number].counter = 1
+            --   color = make_rainbow(event_tick, unit_number, settings, frequency, palette_choice)
             -- end
+            local color = make_rainbow(event_tick, unit_number, frequency, palette_choice)
+            -- local color = {.5,.5,.5}
+            -- local last_position = data.position
+            -- local counter = data.counter
+            -- local current_position = biter.position
+            local surface = biter.surface
+            -- local same_position = last_position and (last_position.x == current_position.x) and (last_position.y == current_position.y)
+            -- local counter_is_less_than_goal = counter and (counter < 15)
+            -- game.print(game.tick.."robot: "..unit_number.." checked again 1")
+            -- if ((not same_position) or (same_position and counter_is_less_than_goal)) then
+              -- game.print(game.tick.."robot: "..unit_number.." checked again 2")
+            -- if biter.effective_speed > 0 then
+            -- if not same_position then
+            if sprite then
+              -- sprite = rendering.draw_sprite{
+              rendering.draw_sprite{
+                sprite = "biter-trail",
+                target = current_position,
+                surface = surface,
+                x_scale = scale,
+                y_scale = scale,
+                render_layer = "radius-visualization",
+                time_to_live = length,
+                tint = color,
+              }
+            end
+            if light then
+              -- light = rendering.draw_light{
+              rendering.draw_light{
+                sprite = "biter-trail",
+                target = current_position,
+                surface = surface,
+                intensity = .175,
+                scale = scale * 1.75,
+                render_layer = "light-effect",
+                time_to_live = length,
+                color = color,
+              }
+            end
+            new_biter_data[unit_number] = {
+              biter = biter,
+              position = current_position,
+              counter = 1
+            }
+          else
+            local counter = data.counter
+            if counter > 555 then
+              global.sleeping_biters[unit_number] = {
+                biter = biter,
+                position = current_position,
+                counter = 1
+              }
+              global.biters[unit_number] = nil
+            else
+              new_biter_data[unit_number] = {
+                biter = biter,
+                position = current_position,
+                counter = counter + 1
+              }
+            end
           end
         end
       end
+      global.biters = new_biter_data
     end
   end
+  -- game.print("active biters: "..table_size(global.biters)..", sleeping biters: "..table_size(global.sleeping_biters))
 end
 
 script.on_event(defines.events.on_tick, function(event)
